d15a3b0d8f86efbe076e35624a4fa8848bc914dd
Gaurav Bhoite <gaurav@velitsolutions.com>
2024-12-03 12:07:41 +0530

    StockController Modification

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -20,37 +20,6 @@ namespace StockLogger.Controllers
     [HttpGet]
     public async Task<IActionResult> GetLatestStockPrice()
     {
-      var requestDto = new GetStockPriceRequestDto
-      {
-        Ticker = "INFY",
-        Exchange = "NSE"
-      };
-
-      var url = $"https://www.google.com/finance/quote/{requestDto.Ticker}:{requestDto.Exchange}";
-
-      var response = await _httpClient.GetStringAsync(url);
-
-      var htmlDoc = new HtmlDocument();
-      htmlDoc.LoadHtml(response);
-
-      var priceNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'YMlKec fxKbKc')]");
-      if (priceNode == null)
-      {
-        return NotFound("Price not found.");
-      }
-
-      var priceText = priceNode.InnerText.Trim();
-      if (!decimal.TryParse(priceText.Substring(1).Replace(",", ""), out var price))
-      {
-        return BadRequest("Failed to parse price.");
-      }
-
-      var stockData = new StockDataDto
-      {
-        time = DateTime.Now.ToString("HH:mm:ss"),
-        price = price
-      };
-
       return View();
     }
   }
